fe8eeffaf8d134446135af748456bb5f5f24e851
Prixix <prixix@vaccum.de>
2019-04-16 00:05:02 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net.Sockets;
+
+namespace Check_Port
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadKey();
+        }
+
+        public static bool CheckOnlineState(String hostname, int port)
+        {
+            bool online;
+
+            using (TcpClient tcpClient = new TcpClient())
+            {
+                try
+                {
+                    tcpClient.Connect(hostname, port);
+                    Console.WriteLine("Connection open");
+                    return true;
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Connection closed");
+                    return false;
+                }
+            }
+        }
+    }
+}
